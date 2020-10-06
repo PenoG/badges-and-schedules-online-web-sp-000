c4d930e62731fead8ff5c05320a1f3961655269d
Mason Phillips <mjphillips2000@gmail.com>
2020-10-06 21:07:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,5 @@ end
 
 def assign_rooms(attendees)
   rooms = []
-  attendees.each_with_index { |name, index| rooms << "Hello, #{name}! " }
+  attendees.each_with_index { |name, index| rooms << "Hello, #{name}! You'll be assigned to room #{index}" }
 end
