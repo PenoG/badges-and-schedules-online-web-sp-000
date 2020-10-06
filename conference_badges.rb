a2c47c6c5d3fe97d9ac142c4542f519394f20aa9
Mason Phillips <mjphillips2000@gmail.com>
2020-10-06 20:50:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each { |name| puts "Hello, my name is #{name}." }
+  list = []
+  attendees.each { |name| "Hello, my name is #{name}." << list }
 end
