3974aed2460eb1b3a0568f72e81bbdfc555ee243
Mason Phillips <mjphillips2000@gmail.com>
2020-10-06 21:15:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each
-  puts assign_rooms(attendees).each
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
